1912a94c08c6c4d2c3709baba4d47bc0929280de
Avichai <avichai@airis-labs.com>
2024-11-10 10:01:05 +0000

    change to knife

--- copy_object_detection_files.py
+++ copy_object_detection_files.py
@@ -9,12 +9,12 @@ def copy_object_detection_json(src_dir, dst_dir):
     # Walk through each folder in the source directory
     for root, dirs, files in os.walk(src_dir):
         # Check if 'object_detection.json' exists in the current folder
-        if "object_detection.json" in files:
+        if "knife_object_detection.json" in files:
             # Get the name of the folder (last part of the path)
             folder_name = os.path.basename(root)
 
             # Construct the full path to the source file
-            src_file = os.path.join(root, "object_detection.json")
+            src_file = os.path.join(root, "knife_object_detection.json")
 
             # Create the destination file name (use the folder name)
             dst_file = os.path.join(dst_dir, f"{folder_name}_object_detection.json")
@@ -26,6 +26,8 @@ def copy_object_detection_json(src_dir, dst_dir):
 
 # Example usage:
 
-source_directory = "/home/ubuntu/Data/obj_det_eval_dataset/videos_frame_samples"
-destination_directory = "/home/ubuntu/Data/obj_det_eval_dataset/object_detection_1000_gun_and_knife_json"
+source_directory = "/home/ubuntu/projects/owl-vit-object-detection-evaluation/data/300_videos_frame_samples"
+destination_directory = (
+    "/home/ubuntu/projects/owl-vit-object-detection-evaluation/data/json/object_detection_300_knife_json"
+)
 copy_object_detection_json(source_directory, destination_directory)
